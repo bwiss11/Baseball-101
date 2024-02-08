169cb4cc9cd61d151fc5638018d72116105c917e
bwiss11 <bwiss11@gmail.com>
2024-02-07 20:06:55 -0500

    Dynamically displaying statistics of all years of a player's career.

--- MLB-React-App/src/pages/Home.jsx
+++ MLB-React-App/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   const [data, setData] = useState(undefined);
 
   const fetchInfo = () => {
-    return fetchStats("Corbin", "Carroll").then((res) => {
+    return fetchStats("Mike", "Trout").then((res) => {
       setData(res);
     });
   };
@@ -27,22 +27,52 @@ const Home = () => {
               <TableHeader />
             </thead>
             <tbody>
-              <TableRow
-                fullData={{
-                  season: data[0][0].season,
-                  hits: data[0][0].stat.hits,
-                  rbis: data[0][0].stat.rbi,
-                  homeruns: data[0][0].stat.homeRuns,
-                  avg: data[0][0].stat.avg,
-                  obp: data[0][0].stat.obp,
-                  slg: data[0][0].stat.slg,
-                  ops: data[0][0].stat.ops,
-                }}
-              />
+              {data[0].map((index) => (
+                <TableRow
+                  fullData={{
+                    season: index.season,
+                    hits: index.stat.hits,
+                    rbis: index.stat.rbi,
+                    homeruns: index.stat.homeRuns,
+                    avg: index.stat.avg,
+                    obp: index.stat.obp,
+                    slg: index.stat.slg,
+                    ops: index.stat.ops,
+                  }}
+                />
+              ))}
             </tbody>
           </table>
         </div>
       </>
+      // <TableRow
+      //     fullData={{
+      //       season: data[0][0].season,
+      //       hits: data[0][0].stat.hits,
+      //       rbis: data[0][0].stat.rbi,
+      //       homeruns: data[0][0].stat.homeRuns,
+      //       avg: data[0][0].stat.avg,
+      //       obp: data[0][0].stat.obp,
+      //       slg: data[0][0].stat.slg,
+      //       ops: data[0][0].stat.ops,
+      //     }}
+      //   />
+      //   <TableRow
+      //     fullData={{
+      //       season: data[0][1].season,
+      //       hits: data[0][1].stat.hits,
+      //       rbis: data[0][1].stat.rbi,
+      //       homeruns: data[0][1].stat.homeRuns,
+      //       avg: data[0][1].stat.avg,
+      //       obp: data[0][1].stat.obp,
+      //       slg: data[0][1].stat.slg,
+      //       ops: data[0][1].stat.ops,
+      //     }}
+      //   />
+      //         </tbody>
+      //       </table>
+      //     </div>
+      //   </>
     );
   }
 };
